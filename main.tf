f8d75828bbf44ed6001d93623497ca1b5db483bd
Honey Modi <Honey>
2021-06-03 17:11:53 -0400

    Uploading the terraform code to git

---
+++ main.tf
@@ -0,0 +1,15 @@
+resource "aws_instance" "HeadInstance" {
+  ami = "ami-0277fbe7afa8a33a6"
+  instance_type = "t2.micro"
+  key_name = "trail-key"
+  tags = {
+      Name = "Terraform-Instance"
+      application = "HeadInstance"
+  }
+}
+
+provider "aws" {
+  region = var.region
+  access_key = var.access_key
+  secret_key = var.secret_key
+}
